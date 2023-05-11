f02470c01cd43e69e49bb7a751c58af66ef6a93d
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2023-05-11 14:44:46 +0100

    fix

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,44 +4,35 @@ import { AuthService } from '../auth.service';
 
 import { ActivatedRoute, Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent {
-   ok : boolean = true
-   activatedRoute!: ActivatedRoute;
+  ok: boolean = true;
+  activatedRoute!: ActivatedRoute;
 
-   constructor(private router : Router , private auth :AuthService){}
-   displayNamee = this.auth.displayName
- 
-       
-   verifie(){
-    this.ok = !this.ok
-    
-    this.router.navigateByUrl('/form')
-   }
-   logout(){
-    alert(this.auth.hasUser())
-   console.log(this.displayNamee)
-    // this.auth.logout().then(res =>{
-    //   alert("Logged out successfully");
-    // }).catch(err => console.log(err));
+  constructor(private router: Router, private auth: AuthService) {}
+  displayNamee = this.auth.displayName;
 
-   }
+  verifie() {
+    this.ok = !this.ok;
 
+    this.router.navigateByUrl('/form');
+  }
+  logout() {
+    // alert(this.auth.hasUser());
+    // console.log(this.displayNamee);
+    this.auth
+      .logout()
+      .then((res) => {
+        alert('Logged out successfully');
+      })
+      .catch((err) => console.log(err));
+  }
 
-
+  goToTemplateCV() {
+    this.router.navigate(['template'], { relativeTo: this.activatedRoute });
+  }
 }
-function resolve(arg0: boolean) {
-  throw new Error('Function not implemented.');
-
-   goToTemplateCV(){
-    this.router.navigate(['template'], {relativeTo : this.activatedRoute})
-   }
-
-
-}
-
